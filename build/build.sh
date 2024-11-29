6dd17e27a6edc9de1e75a2fbd99d7319aa7d02ef
badrm22 <badrm0279@gmail.com>
2024-11-29 10:10:48 +0000

    added bas hscripts to automate build

---
+++ build/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+WORKSPACE=/home/jenkins/jenkins_home/workspace/pipeline-maven
+echo "*********************************************************"
+echo "** Building docker image **"
+echo "*********************************************************"
---
+++ build/mvn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+WORKSPACE=/home/jenkins/jenkins_home/workspace/pipeline-maven
+echo "*****************************************************"
+echo "*** Building JAR ***"
+docker run --rm -it -v /root/.m2:/roo/.m2 \
+	-v $WORKDIR/java-app:/app -w /app \
+	'$@'
+

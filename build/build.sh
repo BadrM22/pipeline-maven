f67b541f12ce57c3d298b64bea8a57279538353d
badrm22 <badrm0279@gmail.com>
2024-11-29 10:20:47 +0000

    setting up docker image automation

--- build/build.sh
+++ build/build.sh
@@ -3,4 +3,5 @@
 WORKSPACE=/home/jenkins/jenkins_home/workspace/pipeline-maven
 echo "*********************************************************"
 echo "** Building docker image **"
+docker compose build --no-cache
 echo "*********************************************************"

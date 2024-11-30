75cc0e8483128dc6fbe0cd1b0ddc3d00805279c4
badrm22 <badrm0279@gmail.com>
2024-11-30 04:20:16 +0000

    fixed typo

--- build/mvn.sh
+++ build/mvn.sh
@@ -3,7 +3,6 @@
 WORKSPACE=/home/jenkins/jenkins_home/workspace/pipeline-maven
 echo "*****************************************************"
 echo "*** Building JAR ***"
-docker run --rm  -v /root/.m2:/root/.m2 \
-	-v $WORKSPACE/java-app:/app -w /app \
-	'$@'
+
+docker run --rm -it -v /root/.m2:/root/.m2 -v $workspace/java-app:/app -w /app maven:latest $@
 

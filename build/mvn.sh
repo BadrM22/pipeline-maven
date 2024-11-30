e8cf36f7661effb8ce18aef12ca06a316c7bb1f9
badrm22 <badrm0279@gmail.com>
2024-11-30 04:26:01 +0000

    fixed

--- build/mvn.sh
+++ build/mvn.sh
@@ -4,5 +4,5 @@ WORKSPACE=/home/jenkins/jenkins_home/workspace/pipeline-maven
 echo "*****************************************************"
 echo "*** Building JAR ***"
 
-docker run --rm -it -v /root/.m2:/root/.m2 -v $workspace/java-app:/app -w /app maven:latest $@
+docker run --rm  -v /root/.m2:/root/.m2 -v $workspace/java-app:/app -w /app maven:latest $@
 
